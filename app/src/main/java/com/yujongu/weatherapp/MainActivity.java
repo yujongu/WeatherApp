ad8fad71e3bc19d47052e9777309b062aa731361
JuhaJeon <jooha6082@gmail.com>
2020-03-24 19:29:15 +0900

    NewBranch Test

--- app/src/main/java/com/yujongu/weatherapp/MainActivity.java
+++ app/src/main/java/com/yujongu/weatherapp/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     RequestQueue rq;
     JsonObjectData jsonData = new JsonObjectData();
     ActivityMainBinding binding;
-    SendJsonRequest sentJsonRequest = new SendJsonRequest();
 
 
     @Override
@@ -37,7 +36,60 @@ public class MainActivity extends AppCompatActivity {
 
         rq = Volley.newRequestQueue(this);
 
-        sentJsonRequest.sentJsonRequest();
+        sentJsonRequest();
     }
 
+    public void sentJsonRequest(){
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=a039162fa2fe09ab61cf60af7145fc65&units=metric";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+
+                    String name = response.getString("name");
+                    jsonData.setName(name);
+                    binding.textviewName.setText(jsonData.getName());
+
+                    JSONObject sys = response.getJSONObject(("sys"));
+                    String country = sys.getString("country");
+                    jsonData.setCountry(country);
+                    binding.textviewCountry.setText(jsonData.getCountry());
+
+                    JSONArray weather = response.getJSONArray("weather");
+                    JSONObject weatherObject = weather.getJSONObject(0);
+                    String main = weatherObject.getString("main");
+                    jsonData.setMain(main);
+                    binding.textviewMain.setText(jsonData.getMain());
+
+                    String description = weatherObject.getString("description");
+                    jsonData.setDescription(description);
+                    binding.textviewDescription.setText(jsonData.getDescription());
+
+                    String icon = weatherObject.getString("icon");
+                    jsonData.setIcon(icon);
+                    binding.textviewIcon.setText(jsonData.getIcon());
+
+                    JSONObject jsonMain = response.getJSONObject("main");
+                    double temp = jsonMain.getDouble("temp");
+                    jsonData.setTemp(temp);
+                    binding.textviewTemp.setText(jsonData.getTemp() + "");
+
+
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        rq.add(jsonObjectRequest);
+    }
 }
