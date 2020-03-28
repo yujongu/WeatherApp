3c379c6cc7156b5b304f31f80b61e2fdc4a7d333
Jongu Yu <yujongu@gmail.com>
2020-03-28 18:18:33 +0900

    Delete api keys
    cuz it's secret

--- app/src/main/java/com/yujongu/weatherapp/OpenWeatherAppKey.java
+++
@@ -1,6 +0,0 @@
-package com.yujongu.weatherapp;
-
-public interface OpenWeatherAppKey {
-    String OPEN_WEATHER_APP_KEY = "a039162fa2fe09ab61cf60af7145fc65";
-    String GOOGLE_PLACES_APP_KEY = "AIzaSyCbd6VTVjVCCv6X85PABgargecUcUNjVms";
-}
