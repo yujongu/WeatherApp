3abfeb3465ab4f606e5d4ac867c0ea3ffdcef83c
JuhaJeon <jooha6082@gmail.com>
2020-03-28 16:22:31 +0900

    SwipeToDelete

--- app/src/main/java/com/yujongu/weatherapp/Firstpage.java
+++ app/src/main/java/com/yujongu/weatherapp/Firstpage.java
@@ -1,21 +1,21 @@
 package com.yujongu.weatherapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 
-import android.media.Image;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
-
 import com.google.android.material.textfield.TextInputEditText;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class Firstpage extends AppCompatActivity {
 
@@ -23,13 +23,15 @@ public class Firstpage extends AppCompatActivity {
     ImageButton imagebutton_add;
     Firstpage_Adapter mAdapter;
     TextInputEditText autoComplete;
+    Firstpage_Data deleted = null;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firstpage);
 
-        RecyclerView mRecyclerView = findViewById(R.id.recyclerview_firstpage);
+        final RecyclerView mRecyclerView = findViewById(R.id.recyclerview_firstpage);
         LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLinearLayoutManager);
 
@@ -52,12 +54,46 @@ public class Firstpage extends AppCompatActivity {
 
                 Firstpage_Data data = new Firstpage_Data(autoComplete.getText().toString());
 
-                //mArrayList.add(0, dict); //RecyclerView의 첫 줄에 삽입
-                mArrayList.add(data); // RecyclerView의 마지막 줄에 삽입
+
+                mArrayList.add(data);
 
                 mAdapter.notifyDataSetChanged();
             }
         });
+
+        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0,  ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                final int position = viewHolder.getAdapterPosition();
+                switch(direction){
+                    case ItemTouchHelper.LEFT:
+                        deleted = mArrayList.get(position);
+                        mArrayList.remove(position);
+                        mAdapter.notifyItemRemoved(position);
+                        break;
+                }
+            }
+            @Override
+            public void onChildDraw (@NonNull Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive){
+
+                new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                        .addSwipeLeftBackgroundColor(ContextCompat.getColor(Firstpage.this, R.color.colorAccent))
+                        .addSwipeLeftActionIcon(R.drawable.ic_delete_black_24dp)
+                        .create()
+                        .decorate();
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(mRecyclerView);
+
+
 
     }
 }
